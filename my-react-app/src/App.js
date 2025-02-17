41983c6e0dcd824dfe4f58f9fee48ec081a2b3d4
Vedanshh14 <vedanshupadhyay1401@gmail.com>
2025-02-17 23:42:52 +0530

    with useEffect

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -2,10 +2,42 @@ import logo from './logo.svg';
 import './App.css';
 import React , {useState , useEffect} from 'react';
 
-function App() {
-  const[size,setSize] = useState(0);
+//.............without Use Effect.........
 
-  
+// function App() {
+//   const[size,setSize] = useState(0);
+
+//   function handleResize(){
+//     setSize(window.innerWidth);
+//   }
+
+//   window.addEventListener('resize',handleResize);
+
+
+//   return (
+//     <div className="App">
+//       <h3>Size is {size}</h3>
+//     </div>
+//   );
+// }
+
+
+function App() {
+  const [size, setSize] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setSize(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    // Cleanup the event listener on component unmount or re-render
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []); // Empty dependency array to run only on mount and unmount
+
   return (
     <div className="App">
       <h3>Size is {size}</h3>
